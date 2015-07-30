d011d0acfa1938f1bbe0c3a4e1e09d0eda44a0aa
Shogo Funaguchi <gooodsun1@gmail.com>
2015-07-30 17:30:37 +0900

    lesson5

---
+++ JavaScript/dotinstall/j5.js
@@ -0,0 +1,17 @@
+var msg = "hello world";
+
+var x = 20;
+x++;
+x +=70;
+var y = 30;
+var kekka;
+kekka = x+y;
+
+var s1 ="hel\tlo S1"
+var s2 ='hello\n S2'
+//カイギョウ|n |n |n
+
+
+
+console.log(s1+s2);
+console.log(s2);
